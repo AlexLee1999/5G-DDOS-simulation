bab3ab1711dd41ba555333be576dc513dfd53952
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-05-04 16:00:51 +0800

    add asp

--- simulation_code/asp.py
+++ simulation_code/asp.py
@@ -7,12 +7,19 @@ from device import Device
 class ASP():
     def __init__(self):
         self.bandwidth = ASP_BANDWIDTH
+        self.frequency = uniform(ASP_CPU_FREQUENCY_LOWER, ASP_CPU_FREQUENCY_UPPER)
         self.device_list = []
         self.num_of_normal_users = randint(ASP_NUM_OF_NORMAL_USERS_LOWER, ASP_NUM_OF_NORMAL_USERS_UPPER)
         self.num_of_malicious_users = randint(ASP_NUM_OF_MALICIOUS_USERS_LOWER, ASP_NUM_OF_MALICIOUS_USERS_UPPER)
+        self.set_users()
+        self.set_service_rate()
 
     def set_users(self):
         for i in range(self.num_of_normal_users):
             self.device_list.append(Device(self.bandwidth / (self.num_of_normal_users + self.num_of_malicious_users)))
 
-a = ASP()
+    def set_service_rate(self):
+        tot_cpu_cycle = 0
+        for dev in self.device_list:
+            tot_cpu_cycle += dev.required_cpu_cycle
+        self.service_rate = self.frequency / (tot_cpu_cycle / (self.num_of_normal_users + self.num_of_malicious_users))
--- simulation_code/device.py
+++ simulation_code/device.py
@@ -13,7 +13,7 @@ class Device():
         self.task_size = uniform(DEVICE_TASK_SIZE_LOWER, DEVICE_TASK_SIZE_UPPER) * 8 * 10E3 ##change to bits
         self.arrival_rate = uniform(DEVICE_ARRIVAL_RATE_LOWER, DEVICE_ARRIVAL_RATE_UPPER)
         self.required_cpu_cycle = uniform(DEVICE_REQUIRED_CPU_CYCLE_LOWER, DEVICE_REQUIRED_CPU_CYCLE_UPPER)
-        self.log_snr = math.log2(1 + self.tx_power * 10E10 * 10**(-(22*math.log10(self.distance)+28+20*math.log10(self.frequency))/10))
+        self.log_snr = math.log2(1 + self.tx_power * 10E10 * 10 ** (-(22 * math.log10(self.distance) + 28 + 20 * math.log10(self.frequency)) / 10))
         self.shannon_rate = self.bandwidth * self.log_snr
         self.transmission_time_to_asp = self.arrival_rate * self.task_size / self.shannon_rate
         self.price_per_task = uniform(DEVICE_PRICE_PER_TASK_LOWER, DEVICE_PRICE_PER_TASK_UPPER)
