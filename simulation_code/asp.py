5aa8fd269e39627b65831b1dd9f43b421b090f9c
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-05-04 13:53:12 +0800

    add asp

--- simulation_code/asp.py
+++ simulation_code/asp.py
@@ -1,7 +1,18 @@
 import math
-import random
+from random import uniform, randint
 from const import *
+from device import Device
 
 
-class asp():
+class ASP():
     def __init__(self):
+        self.bandwidth = ASP_BANDWIDTH
+        self.device_list = []
+        self.num_of_normal_users = randint(ASP_NUM_OF_NORMAL_USERS_LOWER, ASP_NUM_OF_NORMAL_USERS_UPPER)
+        self.num_of_malicious_users = randint(ASP_NUM_OF_MALICIOUS_USERS_LOWER, ASP_NUM_OF_MALICIOUS_USERS_UPPER)
+
+    def set_users(self):
+        for i in range(self.num_of_normal_users):
+            self.device_list.append(Device(self.bandwidth / (self.num_of_normal_users + self.num_of_malicious_users)))
+
+a = ASP()
--- simulation_code/const.py
+++ simulation_code/const.py
@@ -2,11 +2,42 @@
 # These are the parameters of Device
 # Prefix with DEVICE_
 ########################################
+# latency
 DEVICE_LATENCY_UPPER = 100E-3
 DEVICE_LATENCY_LOWER = 5E-3
+# tx_power
 DEVICE_TX_POWER = 199.5262315
+# distance
 DEVICE_DISTANCE_UPPER = 100
 DEVICE_DISTANCE_LOWER = 50
+# frequency
 DEVICE_FREQUENCY = 2.1
+# required cpu cycle
 DEVICE_REQUIRED_CPU_CYCLE_UPPER = 0.6E6
 DEVICE_REQUIRED_CPU_CYCLE_LOWER = 0.5E6
+# task size
+DEVICE_TASK_SIZE_UPPER = 500 ##500 kB
+DEVICE_TASK_SIZE_LOWER = 400 ##400 kB
+# arrival rate
+DEVICE_ARRIVAL_RATE_UPPER = 3
+DEVICE_ARRIVAL_RATE_LOWER = 1
+# price per task
+DEVICE_PRICE_PER_TASK_UPPER = 5 * 10E-3
+DEVICE_PRICE_PER_TASK_LOWER = 10E-3
+
+########################################
+# These are the parameters of ASP
+# Prefix with ASP_
+########################################
+ASP_BANDWIDTH = 20 * 10E6
+ASP_NUM_OF_NORMAL_USERS_UPPER = 100
+ASP_NUM_OF_NORMAL_USERS_LOWER = 50
+ASP_NUM_OF_MALICIOUS_USERS_UPPER = 100
+ASP_NUM_OF_MALICIOUS_USERS_LOWER = 50
+
+
+
+########################################
+# These are the parameters of MPO
+# Prefix with MPO_
+########################################
