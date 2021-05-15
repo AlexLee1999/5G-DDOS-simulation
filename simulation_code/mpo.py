f148a96012a5b5563bbf4bf92eea28b22925910d
AlexLee1999 <chunyen.alex.lee@gmail.com>
2021-05-15 19:31:11 +0800

    fix plot

--- simulation_code/mpo.py
+++ simulation_code/mpo.py
@@ -69,9 +69,8 @@ class MPO():
         return
 
     def plot_phi(self):
-        self.find_optimize_phi()
         phi = 500
-        step = 3
+        step = 2.5
         pr = []
         ut = []
         bound = []
@@ -94,80 +93,22 @@ class MPO():
         plt.xlabel('MPO Price', fontsize=30)
         plt.ylabel('Utility', fontsize=30)
         plt.vlines(self.bd, ymin=min(ut), ymax=max(ut), linestyle='-', color='red', label='Boundary')
-        plt.vlines(self.constraint_phi, ymin=0, ymax=max(ut), linestyle='-', color='darkorange', label='Price Constraint')
-        plt.legend(loc="best")
+        plt.legend(loc="best", fontsize=20)
+        plt.xlabel('MPO Price', fontsize=30)
+        plt.ylabel('Purchased VM', fontsize=30)
         plt.savefig('./utility.jpg')
         plt.close()
         plt.figure(figsize=(20, 16), dpi=100)
-        plt.plot(pr, num, marker='.', linestyle='-.')
+        plt.plot(pr, num, marker='.', linestyle='-.', label='Purchased VM')
         plt.vlines(self.bd, ymin=min(num), ymax=max(num), linestyle='-', color='red', label='Boundary')
-        plt.hlines(self.num_of_vm, xmin=min(700, self.constraint_phi), xmax=500 + 1000 * step, linestyle='-', color='black', label='VM Constraint')
-        plt.vlines(self.constraint_phi, ymin=min(num), ymax=max(num), linestyle='-', color='darkorange', label='Price Constraint')
         plt.title('Total Purchased VM', fontsize=30)
-        plt.legend(loc="best")
+        plt.legend(loc="best", fontsize=20)
         plt.xlabel('MPO Price', fontsize=30)
         plt.ylabel('Purchased VM', fontsize=30)
         plt.savefig('./vm_number.jpg')
         plt.close()
 
-    def find_optimize_phi(self):
-        vm_prior = float('inf')
-        phi_prior = 0.01
-        for bd in self.bd:
-            self.set_and_check_required_vm(bd)
-            vm_after = self.total_vm()
-            phi_after = bd
-            if vm_after < self.num_of_vm and vm_prior > self.num_of_vm:
-                break
-            vm_prior = vm_after
-            phi_prior = phi_after
-        if phi_prior > max(self.qbd):
-            self.constraint_phi = phi_prior
-        else:
-            upper = phi_after
-            lower = phi_prior
-            mid = (upper + lower) / 2
-            self.set_and_check_required_vm(lower)
-            vm_num = self.total_vm()
-            while abs(vm_num - self.num_of_vm) > 0.001 and abs(upper - lower) > 1E-8:
-                mid = (upper + lower) / 2
-                self.set_and_check_required_vm(mid)
-                vm_num = self.total_vm()
-                if vm_num > self.num_of_vm:
-                    lower = mid
-                else:
-                    upper = mid
-            self.set_and_check_required_vm(mid)
-            vm = self.total_vm()
-            self.constraint_phi = mid
 
-    # def plot_malicious_ratio(self):
-    #     ratio = [0.1, 0.3, 0.5, 0.7, 0.9]
-    #     plt.figure(figsize=(20, 16), dpi=100)
-    #     step = 5
-    #     for ra in ratio:
-    #         ut = []
-    #         pr = []
-    #         phi = 500
-    #         for asp in self.asp_lst:
-    #             asp.set_malicious_ratio(ra)
-    #         vm_prior = float('inf')
-    #         for _ in range(1000):
-    #             self.set_and_check_required_vm(phi)
-    #             vm_after = self.total_vm()
-    #             pr.append(phi)
-    #             ut.append(phi * vm_after - MPO_cost(vm_after))
-    #             phi += step
-    #             if vm_prior < vm_after:
-    #                 print('Total VM is not non-increasing')
-    #             vm_prior = vm_after
-    #         plt.plot(pr, ut, marker='.', linestyle='-.', label=f'ratio : {ra}')
-    #     plt.title('Utility of MPO')
-    #     plt.xlabel('MPO Price')
-    #     plt.ylabel('Utility')
-    #     plt.legend(loc="best")
-    #     plt.savefig('./utility_ratio.jpg')
-    #     plt.close()
     def plot_social_welfare(self):
         phi = 500
         step = 2
@@ -264,6 +205,8 @@ class MPO():
         plt.title('Social Welfare', fontsize=30)
         plt.xlabel('MPO Price', fontsize=30)
         plt.ylabel('Utility', fontsize=30)
-        plt.legend(loc="best")
+        plt.xticks(fontsize=20)
+        plt.yticks(fontsize=20)
+        plt.legend(loc="best", fontsize=20)
         plt.savefig('./utility_cmp.jpg')
         plt.close()
